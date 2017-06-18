3accf64655058171cd8684c31de5bfbae1dc78a4
wizardmerlin <yuyankai945@hotmail.com>
2017-06-18 20:16:27 +0800

    update udp

---
+++ code/udp_c_s/client.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+
+#define SERV_PORT 8000
+
+int main(int argc, char *argv[])
+{
+
+  struct sockaddr_in servaddr;
+  int sockfd, n;
+  char buf[BUFSIZ] = {0};
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+
+  servaddr.sin_port = htons(SERV_PORT);
+
+  while(fgets(buf, BUFSIZ, stdin) != NULL) {
+    n = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    if(n==-1){
+      perror("send error");
+    }
+
+    n = recvfrom(sockfd, buf, BUFSIZ, 0, NULL, 0);//do not care the server side
+    if(n==-1){
+      perror("send error");
+    }
+    buf[n]='\0';
+    write(STDOUT_FILENO, buf, n);
+  }
+  
+  
+  return 0;
+}
+
